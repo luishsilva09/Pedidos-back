7337a57bea17757e957a18a0f14233ade82684ab
Luís Henrique <luishsilva09@gmail.com>
2023-04-12 19:48:40 -0300

    feat: middleware para erros

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,11 @@
 import cors from "cors";
 import express from "express";
+import "express-async-errors";
+import { errorHandlerMiddleware } from "./middlewares/errorHandlerMiddleware";
 
 const app = express();
 app.use(cors(), express.json());
+
+app.use(errorHandlerMiddleware);
 
 export default app;
