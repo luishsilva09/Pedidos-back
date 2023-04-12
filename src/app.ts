d8fc20376720346b16fbcf23a816486fc48b2a56
Luís Henrique <luishsilva09@gmail.com>
2023-04-12 20:04:42 -0300

    feat: usando rotas

--- src/app.ts
+++ src/app.ts
@@ -2,10 +2,12 @@ import cors from "cors";
 import express from "express";
 import "express-async-errors";
 import { errorHandlerMiddleware } from "./middlewares/errorHandlerMiddleware";
+import routes from "./routes";
 
 const app = express();
 app.use(cors(), express.json());
 
+app.use(routes);
 app.use(errorHandlerMiddleware);
 
 export default app;
