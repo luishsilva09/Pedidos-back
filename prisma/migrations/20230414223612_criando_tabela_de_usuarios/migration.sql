d8b09f0b0a871e0accf5affe1c0ba831f24414f4
Luís Henrique <luishsilva09@gmail.com>
2023-04-14 19:36:32 -0300

    db: criando tabela de usuarios

---
+++ prisma/migrations/20230414223612_criando_tabela_de_usuarios/migration.sql
@@ -0,0 +1,10 @@
+-- CreateTable
+CREATE TABLE "users" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "isAdmin" BOOLEAN NOT NULL DEFAULT false,
+
+    CONSTRAINT "users_pkey" PRIMARY KEY ("id")
+);
