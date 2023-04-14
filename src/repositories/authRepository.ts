27ef42525c0e05d21c54878d8cd0a58a59aa3b53
Luís Henrique <luishsilva09@gmail.com>
2023-04-14 19:51:48 -0300

    feat: criando usuario

--- src/repositories/authRepository.ts
+++ src/repositories/authRepository.ts
@@ -1,5 +1,10 @@
 import { Prisma } from "@prisma/client";
 import { db } from "../dbStrategy/db";
+
+export async function create(userData: Prisma.UserCreateInput) {
+  return await db.user.create({ data: userData });
+}
+
 export async function findByEmail(email: string) {
   return await db.user.findUnique({ where: { email: email } });
 }
