c565fd9e76476409aea9455b412543c35d7a34cd
Luís Henrique <luishsilva09@gmail.com>
2023-04-14 19:43:26 -0300

    schema: criando user schemas

---
+++ src/schemas/authSchemas.ts
@@ -0,0 +1,13 @@
+import joi from "joi";
+
+export const newUserSchema = joi.object({
+  name: joi.string().trim().required(),
+  email: joi.string().email().trim().required(),
+  password: joi.string().trim().required(),
+  repeatPassword: joi.string().trim().required().valid(joi.ref("password")),
+});
+
+export const signinSchema = joi.object({
+  name: joi.string().trim().required(),
+  email: joi.string().email().trim().required(),
+});
