85eb17fd12239e4b37b3635b5fb5ef399919363b
Luís Henrique <luishsilva09@gmail.com>
2023-04-15 19:25:22 -0300

    fix: mudancas dos dados do schema para signin

--- src/schemas/authSchemas.ts
+++ src/schemas/authSchemas.ts
@@ -8,6 +8,6 @@ export const newUserSchema = joi.object({
 });
 
 export const signinSchema = joi.object({
-  name: joi.string().trim().required(),
   email: joi.string().email().trim().required(),
+  password: joi.string().trim().required(),
 });
