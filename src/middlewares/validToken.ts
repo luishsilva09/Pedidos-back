ae61dd1e7967703e05d437c0f1e7ab5d7d888a27
Luís Henrique <luishsilva09@gmail.com>
2023-04-17 19:53:47 -0300

    feat: middleware de validação de token

---
+++ src/middlewares/validToken.ts
@@ -0,0 +1,23 @@
+import { NextFunction, Request, Response } from "express";
+import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
+import { unauthorizedError } from "../utils/errorUtils";
+
+dotenv.config();
+const secretKey = process.env.SECRET_KEY || "";
+
+export default async function validToken(
+  req: Request,
+  res: Response,
+  next: NextFunction
+) {
+  const { authorization } = req.headers;
+  if (!authorization) throw unauthorizedError("Invalid token");
+
+  const token = authorization.replace("Bearer ", "");
+  jwt.verify(token, secretKey, function (err, decode) {
+    if (err) throw unauthorizedError(err.message);
+  });
+
+  next();
+}
